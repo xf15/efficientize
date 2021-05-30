3ea993b77beeb376f6b5e3b6cd3711c5d84e508a
Xinzhu-Fang <xf15@hampshire.edu>
2021-05-30 00:17:48 -0700

    insignificant commit

--- R/writing.R
+++ R/writing.R
@@ -1,8 +1,4 @@
 
-x = "aea. aegaga. "
-filename = "test.Rmd"
-script = readLines(filename) # each line will be a string. grap strings with period in it. capitalize the first character, and first character 1 space after each period
-
 #' Title
 #' capitalize first letter of first word in each sentence
 #' @param script
@@ -17,13 +13,13 @@ capitalize_sentences = function(script){
   num_str = length(script)
   for (iStr in 1: num_str){
     x = script[iStr]
-    period_positions = str_locate_all(x, "\\.")[[1]]
+    period_positions = stringr::str_locate_all(x, "\\.")[[1]]
     if(length(period_positions) != 0){
       num_sent = nrow(period_positions)
 
-      str_sub(x, 1, 1) = toupper(str_sub(x, 1, 1))
+      stringr::str_sub(x, 1, 1) = toupper(stringr::str_sub(x, 1, 1))
       for(iSent in 1: (num_sent-1)){
-        str_sub(x, period_positions[iSent, "start"]+2, period_positions[iSent, "end"]+2) = toupper(str_sub(x, period_positions[iSent, "start"]+2, period_positions[iSent, "end"]+2))
+        stringr::str_sub(x, period_positions[iSent, "start"]+2, period_positions[iSent, "end"]+2) = toupper(stringr::str_sub(x, period_positions[iSent, "start"]+2, period_positions[iSent, "end"]+2))
       }
       script[iStr] = x
     }
@@ -38,23 +34,42 @@ capitalize_sentences = function(script){
 #' @return
 #' @export
 #'
-#' @examples 
+#' @examples
 #' capitalize_headings("### introduction")
 capitalize_headings = function(script){
   # script = "### introduction"
   num_str = length(script)
   for (iStr in 1: num_str){
     x = script[iStr]
-    pond_positions = str_locate_all(x, "#")[[1]]
+    pond_positions = stringr::str_locate_all(x, "#")[[1]]
     if(length(pond_positions) != 0){
       num_pond = nrow(pond_positions)
 
-      str_sub(x, pond_positions[num_pond, "start"]+2, pond_positions[num_pond, "end"]+2) = toupper(str_sub(x, pond_positions[num_pond, "start"]+2, pond_positions[num_pond, "end"]+2))
+      stringr::str_sub(x, pond_positions[num_pond, "start"]+2, pond_positions[num_pond, "end"]+2) = toupper(stringr::str_sub(x, pond_positions[num_pond, "start"]+2, pond_positions[num_pond, "end"]+2))
       script[iStr] = x
     }
   }
   return(script)
 }
-fileConn = file(filename)
-writeLines(script, fileConn)
-close(fileConn)
+
+# formalize_writing("test.Rmd")
+#' Title
+#'
+#' @param filename
+#'
+#' @return
+#' @export
+#'
+#' @examples
+formalize_writing = function(filename, output_filename = filename){
+  script = readLines(filename) # each line will be a string. grap strings with period in it. capitalize the first character, and first character 1 space after each period
+  script = script %>%
+    capitalize_headings() %>%
+    capitalize_sentences()
+
+  fileConn = file(output_filename)
+  writeLines(script, fileConn)
+  close(fileConn)
+}
+
+
