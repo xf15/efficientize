7fc0cc338268beabbb2cd50e6a59c07b3bcba1bd
Xinzhu-Fang <xf15@hampshire.edu>
2021-05-29 22:19:57 -0700

    insignificant commit

--- R/writing.R
+++ R/writing.R
@@ -1,9 +1,16 @@
-library(tidyverse)
 
 x = "aea. aegaga. "
 filename = "test.Rmd"
 script = readLines(filename) # each line will be a string. grap strings with period in it. capitalize the first character, and first character 1 space after each period
 
+#' Title
+#'
+#' @param script
+#'
+#' @return
+#' @export
+#'
+#' @examples
 capitalize_sentences = function(script){
   # script = "you are human. you really are. "
   num_str = length(script)
@@ -12,7 +19,7 @@ capitalize_sentences = function(script){
     period_positions = str_locate_all(x, "\\.")[[1]]
     if(length(period_positions) != 0){
       num_sent = nrow(period_positions)
-      
+
       str_sub(x, 1, 1) = toupper(str_sub(x, 1, 1))
       for(iSent in 1: (num_sent-1)){
         str_sub(x, period_positions[iSent, "start"]+2, period_positions[iSent, "end"]+2) = toupper(str_sub(x, period_positions[iSent, "start"]+2, period_positions[iSent, "end"]+2))
@@ -23,6 +30,14 @@ capitalize_sentences = function(script){
   return(script)
 }
 
+#' Title
+#'
+#' @param script
+#'
+#' @return
+#' @export
+#'
+#' @examples
 capitalize_headings = function(script){
   # script = "### introduction"
   num_str = length(script)
@@ -31,7 +46,7 @@ capitalize_headings = function(script){
     pond_positions = str_locate_all(x, "#")[[1]]
     if(length(pond_positions) != 0){
       num_pond = nrow(pond_positions)
-      
+
       str_sub(x, pond_positions[num_pond, "start"]+2, pond_positions[num_pond, "end"]+2) = toupper(str_sub(x, pond_positions[num_pond, "start"]+2, pond_positions[num_pond, "end"]+2))
       script[iStr] = x
     }
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(efficientize)
+
+test_check("efficientize")
